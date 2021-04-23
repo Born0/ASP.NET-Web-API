fc1e5c2fd83bf407183ca65394b9e033e5329d4a
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-04-24 03:37:14 +0600

    Rimon's 4th. ProductRepository file added

---
+++ PCHut_API/Repository/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PCHut_API.Repository;
+using PCHut_API.Models;
+
+namespace PCHut_API.Repository
+{
+    public class ProductRepository : Repository<Product>
+    {
+
+    }
+}
